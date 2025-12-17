a1f261e5b9e877b9077d6bc971037bd7302d1abb
Aditya <aditya.2428cseai@gmail.com>
2025-12-17 18:41:09 +0530

    production ready

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chat-app-pnaz.onrender.com/" : "https://chat-app-pnaz.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -19,7 +19,7 @@ export const useAuthStore = create((set, get) => ({
       const res = await axiosInstance.get("/auth/check");
 
       set({ authUser: res.data });
-      get().connectSocket();
+      get().connectSocket();  
     } catch (error) {
       console.log("Error in checkAuth:", error);
       set({ authUser: null });
