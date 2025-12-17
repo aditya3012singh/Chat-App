5a194626fa7fa6060af0880ea22f47321dcde399
Aditya <aditya.2428cseai@gmail.com>
2025-12-17 18:31:10 +0530

    corrected cookie generation

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,8 +7,9 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
+    // secure: process.env.NODE_ENV !== "development",
+    secure: true,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
