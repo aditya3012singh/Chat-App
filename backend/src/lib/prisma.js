630e87a6ff9226d30f4d034b3667999da309901f
Aditya <aditya.2428cseai@gmail.com>
2025-12-17 17:08:24 +0530

    schema version again changed to version 6 and corrected some cors related issues

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,4 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+const globalForPrisma = globalThis;
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "info", "warn", "error"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
 export default prisma;
